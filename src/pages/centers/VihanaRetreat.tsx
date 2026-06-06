33fb145c2afa792bf6d2449e4b5b127d3cdf833b
ruzul2185 <ruzulchaure2185@gmail.com>
2026-06-06 21:09:27 +0530

    Correct breadcrumb - Vihana Retreat Rishikesh Uttarakhand TO Vihana Retreat Hospital Rishikesh Uttarakhand

--- src/pages/centers/VihanaRetreat.tsx
+++ src/pages/centers/VihanaRetreat.tsx
@@ -252,7 +252,7 @@ export default function VihanaRetreat() {
               <ChevronRight className="h-3 w-3 text-primary/20" />
             </li>
             <li className="text-primary/90 font-black shrink-0">
-              Vihana Retreat Rishikesh Uttarakhand
+              Vihana Retreat Hospital Rishikesh Uttarakhand
             </li>
           </ol>
         </div>
